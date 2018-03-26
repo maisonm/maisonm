6dfc584d0f08dd9ae76a15b4fb65ba6cdb032738
maison moa <maison.moa@gmail.com>
2018-03-26 16:30:13 -0400

    new files

--- js/app.js
+++ js/app.js
@@ -1,59 +1,117 @@
-const subBtn = document.getElementById('btn-round1');
-const roundArr = [];
+// ADD SVG BACKGROUND TO HEADER SECTION THAT HAS PARALLAX EFFECT.
+window.onload = () => {
+    parallax(60, headerParallax);
+    calculateSkillSectionTop(70);
+    glyphHoverColor();
+};
 
-subBtn.addEventListener('click', handicapFormula);
+const projectContainer = $('project-card-container');
+const headerParallax = $('.header');
+const hero = $('.hero-container');
+const glyphicons = $('.glyphicon');
 
-function handicapFormula() {
 
-   const fullRoundScore = document.getElementById('full-round-score').value;
-   const courseRating = document.getElementById('course-rating').value;
-   const courseSlope = document.getElementById('course-slope').value;
-   const averageDifficulty = 113;
-   const handicapMath = (fullRoundScore - courseSlope) * averageDifficulty / courseRating;
-   const handicapIndex = Math.round(handicapMath * 100) / 100;
 
-   displayHandicapIndex(handicapIndex);
-   roundArr.push(handicapIndex);
-   roundScore(roundArr);
+// Nav Logo Color Shift
+const navLogoAnimate = () => {
+    let reverse = anime({
+        targets: '.element',
+        complete: function() {
+            anime({
+                targets: '#letter',
+                fill: '#595959',
+                transition: '.1s',
+                delay: 200
+            });
+        }
+    });
+    let color = anime({
+        targets: ['#rect', '#mAlpha'],
+        stroke: '#ed852b',
+        fill: '#ed852b',
+        delay: 1200
+    });
+};
+//
 
+// Parallax Effect
+const parallax = (intensity, el) => {
+        $(window).scroll(function() {
+            let scrollTop = $(window).scrollTop();
+            let position = scrollTop / intensity + 'px';
+            el.css('transform', 'translateY(' + position + ')');
+        })
+    }
+    //
+
+// Navbar Color Theme Change On Scroll
+const calculateSkillSectionTop = (top) => {
+    $(window).on('scroll', () => {
+        let elementOffset = $('.skills-section').offset().top;
+        let scrollTop = $(window).scrollTop();
+        let distance = (elementOffset - scrollTop);
+        if (distance <= top) {
+
+            navLinkColorShift();
+            navBackgroundAdd();
+            navLogoAnimate();
+
+        } else {
+            navLinkColorRevert();
+            navBackgroundRemove();
+        }
+    })
+};
+
+const navLinkColorShift = () => {
+
+    $('.navLinks').addClass('navLinksScrollColor');
 
 };
 
-function roundScore(arr) {
-
-   const round = document.getElementsByClassName('rnd-output');
-
-   for (let i = 0; i <= round.length; i++) {
-
-      if (arr[i]) {
-
-         round[i].innerHTML = arr[i];
-
-      } else if (arr.length === 5) {
-
-         calculateTrueHandicap(arr);
-      } else {
-
-         return false;
-      }
-   }
-}
-
-function calculateTrueHandicap(arr) {
-
-   const averageHandicapOutput = document.getElementById('average-handicap');
-   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-   const differentialSum = arr.reduce(reducer);
-   const handicapCalc = differentialSum / 5;
-   const handicap = Math.round(handicapCalc * 100) / 100;
-
-   averageHandicapOutput.innerHTML = handicap;
+const navLinkColorRevert = () => {
+    $('.navLinks').removeClass('navLinksScrollColor');
 
 }
 
-function displayHandicapIndex(handicap) {
+const navBackgroundAdd = () => {
+    $('.navigation-bar').addClass('navbar-bg');
+}
 
-   const handicapOutput = document.getElementById('handicap');
-   handicapOutput.innerHTML = handicap;
+const navBackgroundRemove = () => {
+        $('.navigation-bar').removeClass('navbar-bg');
+    }
+//
 
-};
+// Input Glyphicon Color Change On Hover 
+
+    const inputName = document.getElementById('name');
+    const inputEmail = document.getElementById('email');
+    const inputMessage = document.getElementById('message');
+
+    function glyphiconColorAdd(){
+
+        glyphicons.addClass('glyphicon-focus');
+    }
+
+    const glyphiconColorRemove = () => {
+
+        glyphicons.removeClass('glyphicon-focus');
+
+    }
+
+    const glyphHoverColor = () => {
+
+        inputName.addEventListener('mouseover', glyphiconColorAdd);
+        inputEmail.addEventListener('mouseover', glyphiconColorAdd);
+        inputMessage.addEventListener('mouseover', glyphiconColorAdd);
+
+        inputName.addEventListener('mouseout', glyphiconColorRemove);
+        inputEmail.addEventListener('mouseout', glyphiconColorRemove);
+        inputMessage.addEventListener('mouseout', glyphiconColorRemove);
+
+
+    }
+
+
+//
